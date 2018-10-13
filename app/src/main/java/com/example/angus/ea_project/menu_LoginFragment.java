e7b711afd442e9f317a0142de394e5cdba3603ba
Angus <angusleung200@gmail.com>
2018-10-13 13:18:57 +0800

    First commit

--- app/src/main/java/com/example/angus/ea_project/menu_LoginFragment.java
+++ app/src/main/java/com/example/angus/ea_project/menu_LoginFragment.java
@@ -6,11 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class menu_LoginFragment extends Fragment {
 
-   
+   EditText et_Userid,et_Userpw;
+    View view;
     public menu_LoginFragment() {
 
     }
@@ -19,11 +21,13 @@ public class menu_LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        view = inflater.inflate(R.layout.fragment_menu__login, container, false);
+        et_Userid = (EditText) view.findViewById(R.id.et_Userid);
+        et_Userpw = (EditText) view.findViewById(R.id.et_Userpw);
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu__login, container, false);
+        return view;
     }
 
 }
