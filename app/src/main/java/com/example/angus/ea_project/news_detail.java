2472bda35309aaf90ed928843f5f08c839a40db4
Angus <angusleung200@gmail.com>
2018-10-18 10:52:13 +0800

    V0.1.1

--- app/src/main/java/com/example/angus/ea_project/news_detail.java
+++ app/src/main/java/com/example/angus/ea_project/news_detail.java
@@ -1,13 +1,39 @@
 package com.example.angus.ea_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class news_detail extends AppCompatActivity {
-
+    String title,description,time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+
+       Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent it = getIntent();
+        Bundle bundle = it.getExtras();
+        title = bundle.getString("title");
+        description =bundle.getString("description");
+        time = bundle.getString("publishedAt");
+        getSupportActionBar().setTitle(title);
+        Log.d("detail", title);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
+
 }
