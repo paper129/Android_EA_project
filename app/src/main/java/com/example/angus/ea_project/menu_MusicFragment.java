95a5753bc8400d41e9b71f0bcb025154bc531ce4
Angus <angusleung200@gmail.com>
2018-11-01 13:49:00 +0800

    V0.1.8

--- app/src/main/java/com/example/angus/ea_project/menu_MusicFragment.java
+++ app/src/main/java/com/example/angus/ea_project/menu_MusicFragment.java
@@ -4,31 +4,52 @@ package com.example.angus.ea_project;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class menu_MusicFragment extends Fragment {
+    SimpleExoPlayerView exoPlayerView;
+    SimpleExoPlayer exoPlayer;
+    String url = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
 
+    public View onCreate(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_menu__music, container, false);
+        exoPlayerView = (SimpleExoPlayerView) view.findViewById(R.id.exo_player);
+        try {
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity().getApplicationContext(), trackSelector);
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String url = "http://rthk.hk/live2.m3u";
-        Uri path = Uri.parse(url);
-
-
-
-
-        /*setContentView(R.layout.activity_main_vv);//***************
-        myVideoView = (VideoView)this.findViewById(R.id.myVideoView);
-        MediaController mc;
-        mc = new MediaController(this);
-        myVideoView.setMediaController(mc);
-        urlStream = "http://rthk.hk/live2.m3u";
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                myVideoView.setVideoURI(Uri.parse(urlStream)); */
-            }
+            Uri radioURI = Uri.parse(url);
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            MediaSource mediaSource = new ExtractorMediaSource(radioURI, dataSourceFactory, extractorsFactory, null, null);
+            exoPlayerView.setPlayer(exoPlayer);
+            exoPlayer.prepare(mediaSource);
+            exoPlayer.setPlayWhenReady(true);
+        }catch (Exception e){
+            Log.d("Error message","Video error");
+        }
+        return view;
+    }
 }
